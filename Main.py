60cc11c48dc4a2f5748b8013b54c4fe05d30eed7
Alessandro <90648026+Aless1997@users.noreply.github.com>
2025-01-18 10:56:13 +0100

    Update Main.py
    Eliminati i commenti

--- Main.py
+++ Main.py
@@ -5,26 +5,26 @@ import pandas as pd
 
 def crea_psw():
     name_s = input('Per quale sito tiserve:\n')
-    #letters to create the basis of the password
+    
     letter_max = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
     letter_min = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
 
-    #selection of sample letters for password
+    
     prima = random.sample(letter_max,8)
     seconda = random.sample(letter_min,8)
 
-    #concatenation of the result of our random lists
+    
     prima_psw = prima+seconda
     random.shuffle(prima_psw)
     psw_ok = "".join(prima_psw)
 
-    #Now with the Bcrypt library, we encrypt our password, an irreversible step
+    
     hashed=bcrypt.hashpw(psw_ok.encode(),bcrypt.gensalt())
     hashed = str(hashed)
 
     hansw = input('Salvare le credenziali in un file? (S/N)\n').strip().upper()
 
-    #Cycle for creating the password saving file
+    
     if hansw == "S":
         lista = os.listdir()
 
